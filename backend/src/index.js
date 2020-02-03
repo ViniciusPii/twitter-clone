125cf4127095d3cd6c4906108542ceff6e854874
Pii <pii@Piis-iMac.local>
2020-02-03 19:01:32 -0300

    Conexão com o banco de dados

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv/config');
 
 const app = express();
 
+mongoose.connect(process.env.MONGOOSE_CONNECT, {
+  useNewUrlParser: true
+});
+
 app.get('/', (req, res) => {
-  return res.send('Olaáááá')
+  return res.send('Opaaaa')
 });
 
 app.listen(3000, () => {
-  console.log('Server started on port 3000')
+  console.log('Server started on port 3000');
 });
