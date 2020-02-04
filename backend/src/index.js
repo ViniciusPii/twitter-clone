0454b26ccaea3570e00e82e763d29f7aeae8824f
Pii <pii@Piis-iMac.local>
2020-02-04 18:39:40 -0300

    Trabalhando com real time

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,10 +4,20 @@ require('dotenv/config');
 
 const app = express();
 
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
 mongoose.connect(process.env.MONGOOSE_CONNECT, {
   useNewUrlParser: true
 });
 
+app.use((req, res, next) => {
+  req.io = io;
+
+  return next();
+});
+
+app.use(express.json());
 app.use(require('./routes'));
 
 app.listen(3000, () => {
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,8 +3,11 @@ const express = require('express');
 const routes = express.Router();
 
 const TweetController = require('./controller/Tweet.controller');
+const LikeController = require('./controller/Like.controller');
 
 routes.get('/tweets', TweetController.index);
 routes.post('/tweets', TweetController.store);
+
+routes.post('/likes/:id', LikeController.store);
 
 module.exports = routes;
