c2b8b9061a09832d65134338d3cc9dbccdd8f58b
Pii <pii@Piis-iMac.local>
2020-02-04 21:39:03 -0300

    Criação da tela de Login

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,8 +1,17 @@
 import React from 'react';
 
+import twitterLogo from '../twitter.svg';
+import './Login.css';
+
 function Login() {
   return (
-    <h1>Login</h1>
+    <div className="login-wrapper">
+      <img src={twitterLogo} alt="GoTwitter"/>
+      <form>
+        <input placeholder="Nome de Usuário"></input>
+        <button type="submit">Entrar</button>
+      </form>
+    </div>
   );
 }
 
